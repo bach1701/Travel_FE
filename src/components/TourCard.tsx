e4c169348b39a0849b342b0fb71b02791f1bf566
bach1701 <phamduybach1007@gmail.com>
2025-04-21 21:03:30 +0700

    home: fix run  build

--- src/components/TourCard.tsx
+++ src/components/TourCard.tsx
@@ -3,6 +3,8 @@ import { Tour } from '@/types/Tour';
 import { FaClock } from 'react-icons/fa';
 import { FaPlane } from 'react-icons/fa6';
 import { FaPaperPlane } from 'react-icons/fa';
+import { Card, CardContent } from './ui/Card';
+import { Badge } from './ui/Badge';
 
 interface TourCardProps {
   tour: Tour;
